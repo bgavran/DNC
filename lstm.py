5d25184587d6285ba6491ccd8f0c2fa5200c1f9e
bgavran3 <bruno.gavranovic@fer.hr>
2017-03-25 23:50:02 +0100

    Added visualization for cell state, for all time steps

--- lstm.py
+++ lstm.py
@@ -14,12 +14,17 @@ class LSTM(Controller):
 
         # Extra layer of neural network
         # The output is a vector of dimension memory_size, but we want it to be output_size, so we multiply it by some W
-        self.weights = tf.Variable(tf.random_normal([self.memory_size, self.output_size], stddev=0.1))
-        self.biases = tf.Variable(tf.zeros([self.output_size]))
+        self.weights = tf.Variable(tf.random_normal([self.memory_size, self.output_size], stddev=0.1),
+                                   name="output_weights")
+        self.biases = tf.Variable(tf.zeros([self.output_size]), name="output_biases")
 
     def __call__(self, inputs, sequence_length=None):
-        outputs, state = tf.contrib.rnn.static_rnn(self.lstm_cell, inputs, sequence_length=sequence_length,
-                                                   dtype=tf.float32)
+        outputs, states = tf.contrib.rnn.static_rnn(self.lstm_cell, inputs, sequence_length=sequence_length,
+                                                    dtype=tf.float32)
+        # states = tf.transpose(states, [1, 3, 0, 2])
+        states = tf.reshape(tf.transpose(states, [2, 3, 1, 0]),
+                            [self.batch_size, 2 * self.memory_size, self.max_seq_length, 1])
+        tf.summary.image("LSTM hidden state (two of them)", states, max_outputs=2 * self.batch_size)
         outputs = [tf.matmul(o, self.weights) + self.biases for o in outputs]
         outputs = tf.transpose(outputs, [1, 2, 0])
-        return outputs, state
+        return outputs, states[-1]
--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import tensorflow as tf
 from utils import *
 from lstm import *
 from tasks import *
+from tensorflow.python.ops import array_ops
 
 batch_size = 5
-input_size = 3
-output_size = 3
-memory_size = 32
+input_size = 2
+output_size = input_size
+memory_size = 8
 seq_length = 4
 total_sequences_length = 2 * seq_length + 2
 
@@ -18,13 +19,26 @@ y = tf.placeholder(tf.float32, [None, input_size, total_sequences_length], name=
 
 x_list = tf.split(x, total_sequences_length, axis=2)
 x_list = [tf.squeeze(i, 2) for i in x_list]
+
 outputs, _ = controller(x_list)
+tf.summary.image("Input", tf.expand_dims(x, axis=3), max_outputs=batch_size)
+tf.summary.image("Output", tf.expand_dims(outputs, axis=3), max_outputs=batch_size)
+weights_expanded = tf.expand_dims(tf.expand_dims(controller.weights, axis=0), axis=3)
+# tf.summary.image("LSTM weights", tf.get_variable("output_weights"), max_outputs=batch_size)
+tf.summary.image("LSTM output weights", tf.transpose(weights_expanded), max_outputs=batch_size)
+
+inner_weights = [v for v in tf.global_variables() if v.name.startswith("rnn/basic_lstm_cell/weights")][0]
+inner_weights_expanded = tf.expand_dims(tf.expand_dims(inner_weights, axis=0), axis=3)
+# i, j, f, o = array_ops.split(value=inner_weights, num_or_size_splits=4, axis=1)
+# input_weights = tf.expand_dims(tf.expand_dims(i, axis=0), axis=3)
+tf.summary.image("LSTM inner weights", tf.transpose(inner_weights_expanded), max_outputs=batch_size)
+
 cost = tf.reduce_mean(tf.square(y - outputs))
 optimizer = tf.train.AdamOptimizer().minimize(cost)
 
-tf.summary.scalar('cost', cost)
+tf.summary.scalar('Cost', cost)
 merged = tf.summary.merge_all()
-training_steps = 1000
+training_steps = 100000
 with tf.Session() as sess:
     train_writer = tf.summary.FileWriter(os.path.join(DataPath.base, "train_summary"), sess.graph)
     tf.global_variables_initializer().run()
@@ -32,7 +46,7 @@ with tf.Session() as sess:
     for step in range(training_steps):
         batch_x, batch_y = CopyTask.generate_values(batch_size, input_size, seq_length)
         sess.run(optimizer, feed_dict={x: batch_x, y: batch_y})
-        if step % 100 == 0:
+        if step % 1000 == 0:
             print("Training...", step)
             summary = sess.run(merged, feed_dict={x: batch_x, y: batch_y})
             train_writer.add_summary(summary, step)
